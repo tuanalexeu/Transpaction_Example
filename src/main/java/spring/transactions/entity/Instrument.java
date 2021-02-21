9484d25b6032b5ddb65a862a00e1b25251e122c1
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-21 12:48:30 +0300

    Create model

---
+++ src/main/java/spring/transactions/entity/Instrument.java
@@ -0,0 +1,42 @@
+package spring.transactions.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "instrument")
+public class Instrument implements Serializable {
+
+    @Id
+    @Column(name = "INSTRUMENT_ID")
+    private String instrumentId;
+
+    @ManyToMany
+    @JoinTable(name = "singer_instrument",
+            joinColumns = @JoinColumn(name = "INSTRUMENT_ID"),
+            inverseJoinColumns = @JoinColumn(name = "SINGER_ID"))
+    private Set<Singer> singers = new HashSet<>();
+
+    public String getInstrumentId() {
+        return instrumentId;
+    }
+
+    public void setInstrumentId(String instrumentId) {
+        this.instrumentId = instrumentId;
+    }
+
+    public Set<Singer> getSingers() {
+        return singers;
+    }
+
+    public void setSingers(Set<Singer> singers) {
+        this.singers = singers;
+    }
+
+    @Override
+    public String toString() {
+        return "Instrument :" + getInstrumentId();
+    }
+}
